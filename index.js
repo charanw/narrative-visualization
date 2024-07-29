17ee790a013f9ccb041df83c1b4eb4de613bf225
charanw <charanwilliams@gmail.com>
2024-07-28 21:49:41 -0500

    Minor text fix

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ export const MESSAGE = `<p>According to the Intergovernmental Panel on Climate C
             is "unequivocal" that, primarily by greenhhouse gas emissions, human
             activity has caused global warming, leading to "widespread and rapid
             changes in the atmosphere, ocean, cryosphere, and biosphere" with
-            "adverse impacts". (IPCC, 2023) This narrative visualization
+            "adverse impacts." (IPCC, 2023) This narrative visualization
             explores the evolution of global CO2 (a greenhouse gas) emissions
             and how emissions have been impacted by major world events. Please
             click the next button below to progress through the visualization.</p>`;
